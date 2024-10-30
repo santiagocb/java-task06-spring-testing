29a3acc867cceae3e8fffcc81199193dc73a948d
Santiago <santiagocadavidbte@gmail.com>
2024-10-29 23:56:57 -0500

    Refactor imports

--- src/test/java/com/ticketland/integration/BookingControllerTest.java
+++ src/test/java/com/ticketland/integration/BookingControllerTest.java
@@ -4,24 +4,16 @@ import com.ticketland.controllers.BookingController;
 import com.ticketland.entities.Event;
 import com.ticketland.entities.Ticket;
 import com.ticketland.entities.User;
-import com.ticketland.entities.UserAccount;
-import com.ticketland.exceptions.InsufficientFundsException;
 import com.ticketland.facades.BookingFacade;
 import com.ticketland.jms.JmsConsumer;
 import com.ticketland.jms.JmsProducer;
-import jakarta.persistence.EntityNotFoundException;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
-
 
 import java.time.LocalDate;
 import java.util.List;
@@ -29,7 +21,6 @@ import java.util.concurrent.TimeUnit;
 
 import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
